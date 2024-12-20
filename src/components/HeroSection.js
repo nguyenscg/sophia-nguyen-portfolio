ceda95ae1d3433ec277c42e5bbad4f2ffc2041ba
nguyenscg <nguyencsg@gmail.com>
2024-12-20 17:28:37 -0600

    created HeroSection component and rendered

---
+++ src/components/HeroSection.js
@@ -0,0 +1,8 @@
+function HeroSection() {
+    return (
+        <><h1>Hello, I'm Sophia Nguyen!</h1>
+        </>
+    )
+}
+
+export default HeroSection;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,16 @@
 function NavBar() {
     return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#"><img src={''} alt="logo"/></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
-                <a className="nav-link active">Home <span class="sr-only">(current)</span></a>
-                <a className="nav-link">About</a>
-                <a className="nav-link">Skills</a>
-                <a className="nav-link disabled">Contact</a>
+                <a className="nav-link active" href="#home">Home <span class="sr-only">(current)</span></a>
+                <a className="nav-link" href="#about">About</a>
+                <a className="nav-link" href="#skills">Skills</a>
+                <a className="nav-link" href="#projects">Projects</a>
             </div>
         </div>
     </nav>
