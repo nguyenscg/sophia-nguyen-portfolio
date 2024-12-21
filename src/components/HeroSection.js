f0bfdff9a23b201731323f1fe92cef77c1c5503a
nguyenscg <nguyencsg@gmail.com>
2024-12-20 18:01:26 -0600

    added section and container for hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,12 @@
 function HeroSection() {
     return (
-        <><h1>Hello, I'm Sophia Nguyen!</h1>
+        <>
+            <section className="hero" id="none"></section>
+            <div className="container">
+                <span className="tagline">Welcome to my Portfolio!</span>
+                <h1>Hello, I'm Sophia Nguyen! <span className="wrap">Software engineer & aspiring data scientist</span></h1>
+                <p>with a passion for crafting interactive web applications and analyzing data to drive impactful solutions. Whether it's writing clean code or solving complex problems, I thrive on creating experiences that engage and inspire users.</p>
+            </div>
         </>
     )
 }
