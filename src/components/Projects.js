7975a833fd0b28eb1fba5bfcbc1502f098d40498
nguyenscg <nguyencsg@gmail.com>
2025-01-29 12:41:27 -0600

    created projects component for each project to be a card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,28 @@
-function Projects() {
+import React from "react";
+
+const Projects = () => {
     return (
-        <>
-        <h3>Projects will go here</h3>
-        </>
-    )
+        <div className="row row-cols-1 row-cols-md-3 g-4">
+            <div className="col">
+                <div className="card h-100">
+                    <img src="..." className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Game Analytics Dashboard</h5>
+                        <p className="card-text">This is a longer card with...</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                    <img src="..." className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Game Analytics Dashboard</h5>
+                        <p className="card-text">This is a longer card with...</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Projects;
