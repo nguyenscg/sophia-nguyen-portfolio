ce61376e125f64f93d1a04fec4ce24163ce89b5e
Sophia Nguyen <nguyencsg@gmail.com>
2025-01-29 00:29:25 -0600

    fixed navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,19 @@
-function NavBar() {
+import React, { useEffect, useState } from "react";
+const NavBar = () => {
+    const [active, setActive] = useState("");
+
+    const updateActive = (value) => {
+        setActive(value);
+    }
+    
     return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#"><img src={''} alt="logo"/></a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-                <a className="nav-link active" href="#home">Home <span class="sr-only">(current)</span></a>
-                <a className="nav-link" href="#about">About</a>
-                <a className="nav-link" href="#skills">Skills</a>
-                <a className="nav-link" href="#projects">Projects</a>
-            </div>
-        </div>
-    </nav>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#"><img src={''} alt="logo"/></a>
+                    <a href="#home" className={`nav-link ${active === "home" ? "active" : ""}`} onClick={() => updateActive("home")}>Home</a>
+                    <a href="#about" className={`nav-link ${active === "about" ? "active" : ""}`} onClick={() => updateActive("about")}>About</a>
+                    <a href="#projects" className={`nav-link ${active === "projects" ? "active" : ""}`} onClick={() => updateActive("projects")}>Projects</a>
+                    <a href="#contact" className={`nav-link ${active === "contact" ? "active" : ""}`} onClick={() => updateActive("contact")}>Contact</a>
+        </nav>
     );
 }
-
 export default NavBar;
