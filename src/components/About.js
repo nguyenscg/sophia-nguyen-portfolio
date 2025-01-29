a52ac9e90b1147c59bad167633e307ce72ff8a76
nguyenscg <nguyencsg@gmail.com>
2025-01-29 12:52:30 -0600

    added more text to about component

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,11 @@ const About = () => {
         <div class="card-body">
             Hi, I'm Sophia, a Python Developer & Aspiring Data Scientist with a background in software engineering. My passion lies in building data-driven applications, analyzing complex datasets,
             and creating backend solutions that solve real-world problems.
+
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   </div>
 </div>
     )
