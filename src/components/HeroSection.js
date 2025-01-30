97ae9f84ec421522acb32f4faafad0e9ad4b7f47
nguyenscg <nguyencsg@gmail.com>
2025-01-30 17:02:17 -0600

    added color styling, text spacing and adjusting size in hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,18 +2,14 @@ import React from "react";
 
 const HeroSection = () => {
     return (
-        <>
-            <section className="hero" id="none"></section>
-            <div className="container">
-                <span className="tagline">Welcome to my Portfolio!</span>
-                <h1>Hello, I'm Sophia Nguyen! <span className="wrap">Python Developer & Aspiring Data Scientist</span></h1>
-                <p>Passionate about building data-driven solutions and backend applications. I enjoy working with Python, Flask, and data analytics to transform raw data into meaningful insights.</p>
-                <div className="flex justify-center items-center h-screen bg-blue-500">
-      <h1 className="text-4xl text-white font-bold">Hello, Tailwind CSS!</h1>
-    </div>
-            </div>
-        </>
-    )
+        <section className="relative w-full h-screen flex flex-col items-center justify-center bg-deepPurple text-white text-center p-6">
+            <span className="text-neonCyan text-lg font-semibold uppercase tracking-wider">Welcome to my Portfolio!</span>
+            <h1 className="text-5xl md:text-6xl font-bold mt-4">Hello, I'm <span className="text-valorantRed">Sophia Nguyen</span></h1>
+            <h2 className="text=2xl md:text-3xl font-semibold mt-2">Python Developer & <span className="text-neonCyan">Aspiring Data Scientist</span></h2>
+            <p className="text-lg max-w-2xl mt-4 text-gray-300">Passionate about building data-driven solutions and backend applications. 
+                I enjoy working with Python, Flask, and data analytics to transform raw data into meaningful data.</p>
+        </section>
+    );
 }
 
 export default HeroSection;
