314d4399fe4c867f35df631d6bdec245c3ca9f08
Sophia <nguyencsg@gmail.com>
2025-11-06 16:07:06 -0600

    updated colors for hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        neonCyan: "#00E6E6",
+        primaryRose: "#F4C2C2",
+        background: "#1F0B0A",
+        subtitles: "#C79898",
+        highlights: "#734A4A",
+        buttons: "#4C2628",
+        hmm: "#9C7070",
         valorantRed: "#FF4655",
         cloudMist: "#FAF9F6",
-        rose: "#F4C2C2",
         midnight: "#2F2F3A",
         deepPurple: "#2E1A47",
         dusty: "#B8AFA1",
